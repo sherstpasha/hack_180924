2af369cbd409fa4661c83095bbe47c7d03570e8e
asmikhalev <38465867+asmikhalev@users.noreply.github.com>
2024-09-29 11:41:21 +0700

    Update test_predict.py

--- test_predict.py
+++ test_predict.py
@@ -8,7 +8,7 @@ def parse_arguments():
     
     parser.add_argument('--model_folder_path', type=str, required=True, help="Путь до папки с моделью")
     parser.add_argument('--video_folder_path', type=str, required=True, help="Путь до папки с видео")
-    
+    parser.add_argument('--label_list_path', type=str, required=True, help="Путь до файла с названиями меток")
     parser.add_argument('--title', type=str, default="Example Video Title", help="Название видео")
     parser.add_argument('--description', type=str, default="This is an example description of the video.", help="Описание видео")
 
@@ -22,14 +22,19 @@ if __name__ == "__main__":
     # Устройство (CPU или GPU)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # Считывание списка меток из файла
+    with open(args.label_list_path, "r", encoding="utf-8") as f:
+        label_list = [line.strip() for line in f.readlines()]
+
     # Получение предсказанных меток
     predicted_tags = predict_single_video(
         model_path=args.model_folder_path,
         video_path=args.video_folder_path,
         title=args.title,
         description=args.description,
-        label_list=None, 
+        label_list=label_list, 
         device=device
     )
     
     print(f"Предсказанные метки: {predicted_tags}")
+
